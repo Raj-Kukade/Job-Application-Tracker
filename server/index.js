5aab3f21017fffd2fc450b59b35fbc96a9cc0558
Raj Kukade <157631623+Raj-Kukade@users.noreply.github.com>
2025-07-04 14:23:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(cors({
+  origin: 'https://job-application-tracker-frontend-rdzy.onrender.com',
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
